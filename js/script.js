fe84f96c3edb20d96ef9741ae768b45af30b4c61
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-07-31 22:34:58 +0300

    added getpizza constructor

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// business logic 
+var price, crustPrice,toppingPrice
+let cost=0;
+function getPizza(name,size,crust,topping,total){
+    this.name=name;
+    this.size=size;
+    this.crust=crust;
+    this.topping=topping;
+    this.total=total;
+}
+
+// user interface logic
+$(document).ready(function(){
+  $("button#order").click(function(event){
+      event.preventDefault();
+      let pizzaName=$(".name option:selected").val();
+      let pizzaSize=$("#size option:selected").val();
+      let PizzaCrust=$("#crust optio:selected").val();
+  })  
+})
+ 
