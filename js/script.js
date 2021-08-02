cb1ea3cf81da05e60a1647aebd8ad4eb533070cb
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-08-02 06:42:40 +0300

    first form validation

--- js/script.js
+++ js/script.js
@@ -11,36 +11,46 @@ function getPizza(name, size, crust, topping, total) {
 };
 
 // user interface logic
-$(document).ready(function () {
-  $("button#order").click(function (event) {
-    event.preventDefault();
-    let pizzaName = $(".name option:selected").val();
-    let pizzaSize = $("#size option:selected").val();
-    let PizzaCrust = $("#crust optio:selected").val();
-    let pizzaTopping = [];
+function order(){
+  var type=document.getElementById("ordersmade").value;
+    if(myForm.type.value==""){
+      alert("please select your flovor");
+      return false;
+    }
+    else if(myForm.size.value=="")
+}
 
-    function.(element => {
+// $(document).ready(function () {
+//   $("button#order").click(function (event) {
+//     event.preventDefault();
+//     let pizzaName = $(".name option:selected").val();
+//     let pizzaSize = $("#size option:selected").val();
+//     let PizzaCrust = $("#crust optio:selected").val();
+//     let pizzaTopping = [];
 
-    });
+//     function.(element => {
 
-  });
-  let toppingValue = pizzaTopping.length * 60;
-  console.log("toppings value" + toppingsValue);
+//     });
+
+//   });
+//   let toppingValue = pizzaTopping.length * 60;
+//   console.log("toppings value" + toppingsValue);
 
   
-    if (pizzaSize == 0 && PizzaCrust == 0) {
-      console.log("nothing selected");
-      $("button#order").show();
-      $("button#information").show();
-      $("button.choice").hide();
-      alert("please select pizza size and crust")
-    } else {
-      $("button#order").hide();
-      $("button#information").hide();
-      $("button.choice").slideDown(800);
-    }
+//     if (pizzaSize == 0 && PizzaCrust == 0) {
+//       console.log("nothing selected");
+//       $("button#order").show();
+//       $("button#information").show();
+//       $("button.choice").hide();
+//       alert("please select pizza size and crust")
+//     } else {
+//       $("button#order").hide();
+//       $("button#information").hide();
+//       $("button.choice").slideDown(800);
+//     }
     
-    total=price+crustPrice+toppingValue;
-    console.log(total);
-    let checkoutTotal=checkoutTotal+total;
-  })
+//     total=price+crustPrice+toppingValue;
+//     console.log(total);
+//     let checkoutTotal=checkoutTotal+total;
+//   })
+  
